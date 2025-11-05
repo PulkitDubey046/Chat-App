9b774d21a10bf920a7b58fd6102bf95c43f69a25
Pulkit Dubey <46dubey@gmail.com>
2025-11-05 15:06:19 +0530

    Add routing and page components for Home, Login, and Profile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage.jsx'
+import LoginPage from './pages/LoginPage.jsx'
+import ProfilePage from './pages/ProfilePage.jsx'
+import bgImage from './assets/bgImage.svg'
 
 const App = () => {
   return (
-    <div>
-      
+    <div className={`bg-[url(${bgImage})] bg-cover min-h-screen`}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
     </div>
   )
 }
